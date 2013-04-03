0896d267c4f17b9d4a86b94cfda2c1ff734dca89
Arto Jalkanen <ajalkane@gmail.com>
2013-04-03 22:39:58 +0300

    parentPath is property again.

--- folderlistmodel/dirmodel.h
+++ folderlistmodel/dirmodel.h
@@ -82,6 +82,7 @@ public:
     Q_PROPERTY(QString path READ path WRITE setPath NOTIFY pathChanged);
     inline QString path() const { return mCurrentDir; }
 
+    Q_PROPERTY(QString parentPath READ parentPath NOTIFY pathChanged);
     Q_INVOKABLE QString parentPath() const;
 
     Q_INVOKABLE QString homePath() const;
